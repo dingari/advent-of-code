e0d7f19478ea0e13dc2f209e50c2be6473d647f8
Daniel Gretarsson <danielgretars@gmail.com>
2019-12-02 18:21:12 +0000

    Day1 + gitignore

---
+++ y2019/src/day1/mod.rs
@@ -0,0 +1,31 @@
+use super::parse_lines;
+
+fn calculate_fuel(x: &i32) -> i32 { x / 3 - 2 }
+
+fn calculate_fuel_rec(x: &i32) -> i32 {
+    let f = calculate_fuel(x);
+    if f <= 0 { 0 } else { f + calculate_fuel_rec(&f) }
+}
+
+pub fn run(input_str: &String) {
+    let input = parse_lines(
+        &input_str.trim_end_matches('\n').to_string(),
+        |s| s.parse::<i32>().unwrap(),
+    );
+
+    // Part 1
+    assert_eq!(calculate_fuel(&12), 2);
+    assert_eq!(calculate_fuel(&14), 2);
+    assert_eq!(calculate_fuel(&1969), 654);
+    assert_eq!(calculate_fuel(&100756), 33583);
+
+    let sum1: i32 = input.iter().map(calculate_fuel).sum();
+    println!("Part 1: {:?}", sum1);
+
+    // Part 2
+    assert_eq!(calculate_fuel_rec(&14), 2);
+    assert_eq!(calculate_fuel_rec(&1969), 966);
+
+    let sum2: i32 = input.iter().map(calculate_fuel_rec).sum();
+    println!("Part 2: {:?}", sum2);
+}
---
+++ y2019/src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+
+mod day1;
+
+fn read_file_to_string(file_path: &Path) -> String {
+    let mut file = File::open(file_path).unwrap();
+    let mut contents = String::new();
+
+    let _len = file.read_to_string(&mut contents).unwrap();
+
+    contents
+}
+
+fn parse_lines<T, F>(input: &String, parser: F) -> Vec<T> where F: Fn(&str) -> T {
+    input.split("\n").map(|s| parser(s)).collect()
+}
+
+fn main() {
+    let root = env::current_dir().unwrap().join("src");
+    println!("Current dir: {:?}", root.to_str());
+
+    day1::run(&read_file_to_string(root.join("day1").join("input.txt").into_boxed_path().as_ref()));
+}
