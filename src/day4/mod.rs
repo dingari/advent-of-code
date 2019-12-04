4e5817017b794ada648899917a3b31aca7ee6d58
Daniel Gretarsson <danielgretars@gmail.com>
2019-12-04 18:05:25 +0000

    Making things prettier

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -6,6 +6,12 @@ pub fn run(input_str: &String) {
     let v = input_str.trim_end_matches('\n').split('-').collect::<Vec<&str>>();
     let (begin, end) = (v[0].parse::<usize>().unwrap(), v[1].parse::<usize>().unwrap());
 
+    let count_valid_passwords = |begin: usize, end: usize, f: &dyn Fn(&str) -> bool| -> usize {
+        (begin..=end)
+            .filter(|x| f(&x.to_string()))
+            .count()
+    };
+
     //==============================================================================================
     // Part 1
     let is_valid_password = |pw: &str| -> bool {
@@ -22,23 +28,16 @@ pub fn run(input_str: &String) {
     assert_eq!(is_valid_password("223450"), false);
     assert_eq!(is_valid_password("123789"), false);
 
-    let num_valid_passwords = (begin..=end)
-        .fold(0_usize, |acc, x| {
-            if is_valid_password(&x.to_string()) { acc + 1 } else { acc }
-        });
-
-    println!("Part 1: {}", num_valid_passwords);
+    println!("Part 1: {}", count_valid_passwords(begin, end, &is_valid_password));
 
     //==============================================================================================
     // Part 2
     let is_valid_password_2 = |pw: &str| -> bool {
-        let groups = pw.chars()
+        let has_strict_duplicate = pw.chars()
             .group_by(|&x| x)
             .into_iter()
-            .map(|(_, r)| r.collect())
-            .collect::<Vec<String>>();
-
-        let has_strict_duplicate = groups.iter().any(|s| s.len() == 2);
+            .map(|(_, r)| r.collect::<String>())
+            .any(|s| s.len() == 2);
 
         is_valid_password(&pw) && has_strict_duplicate
     };
@@ -47,11 +46,6 @@ pub fn run(input_str: &String) {
     assert_eq!(is_valid_password_2("123444"), false);
     assert_eq!(is_valid_password_2("111122"), true);
 
-    let num_valid_passwords_2 = (begin..=end)
-        .fold(0_usize, |acc, x| {
-            if is_valid_password_2(&x.to_string()) { acc + 1 } else { acc }
-        });
-
-    println!("Part 2: {}", num_valid_passwords_2);
+    println!("Part 2: {}", count_valid_passwords(begin, end, &is_valid_password_2));
 }
 
