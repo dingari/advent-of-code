527dc24584295a5f456d838372aa4cd089c0fbc7
Daníel Grétarsson <danielgretars@bitbucket.com>
2019-12-11 21:15:44 +0000

    Day 10 - part 2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,9 +1,10 @@
 use std::collections::{HashSet, HashMap};
+use std::f32::consts::PI;
+use std::cmp::Ordering;
 
 use num::Complex;
 use itertools::Itertools;
 
-//type P = (i32, i32);
 type P = Complex<i32>;
 
 fn parse(input: &str) -> HashSet<P> {
@@ -22,11 +23,11 @@ fn parse(input: &str) -> HashSet<P> {
         .collect::<HashSet<P>>()
 }
 
-fn visible_asteroids(asteroids: &HashSet<P>) -> HashMap<P, HashSet<P>> {
-    let to_float = |c: Complex<i32>| -> Complex<f32> {
-        Complex::<f32>::new(c.re as f32, c.im as f32)
-    };
+fn to_float(c: Complex<i32>) -> Complex<f32> {
+    Complex::<f32>::new(c.re as f32, c.im as f32)
+}
 
+fn max_visibility(asteroids: &HashSet<P>) -> Option<(P, usize)> {
     asteroids
         .iter()
         .map(|&c| (c, asteroids
@@ -34,35 +35,73 @@ fn visible_asteroids(asteroids: &HashSet<P>) -> HashMap<P, HashSet<P>> {
                 .into_iter()
                 .collect::<HashSet<P>>()
             )
-            .map(|&p| (p, to_float(p - c).arg()))
-            .sorted_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
-            .group_by(|&x| x.1)
-            .into_iter()
-            .map(|(_, r)| r.collect::<Vec<_>>()
-                .into_iter()
-                .min_by(|a, b| (a.0 - c).norm_sqr().cmp(&(b.0 - c).norm_sqr()))
-            )
-            .filter(|o| o.is_some())
-            .map(|o| o.unwrap().0)
-            .collect::<HashSet<_>>()
+            .map(|&p| to_float(p - c).arg())
+            .sorted_by(|&a, &b| a.partial_cmp(&b).unwrap_or(Ordering::Equal))
+            .dedup()
+            .count()
         ))
-        .collect::<HashMap<P, HashSet<P>>>()
+        .max_by(|&(_, a), &(_, b)| a.cmp(&b))
 }
 
-fn max_visibility(asteroids: &HashSet<P>) -> Option<(P, usize)> {
-    match visible_asteroids(asteroids).iter().max_by(|&v1, &v2| v1.1.len().cmp(&v2.1.len())) {
-        Some((&k, v)) => Some((k, v.len())),
-        None => None
+fn lazer(source: P, asteroids: &HashSet<P>, target: usize) -> Option<P> {
+    let unwrap_phase = |p: f32| if p < 0.0 { p + 2.0 * PI } else { p };
+
+    let mut remaining = asteroids.clone();
+    let mut i = 0;
+
+    while remaining.len() > 1 {
+        let closest = remaining
+            .difference(&vec![source]
+                .into_iter()
+                .collect::<HashSet<P>>()
+            )
+            .map(|&p| (p, unwrap_phase(to_float((p - source) * P::new(0, 1)).arg()), (p - source).norm_sqr()))
+            .sorted_by(|&a, &b| a.1.partial_cmp(&b.1).unwrap())
+            .group_by(|&x| x.1)
+            .into_iter()
+            .map(|(_, r)| r.into_iter().min_by(|a, b| (a.0 - source).norm_sqr().cmp(&(b.0 - source).norm_sqr())))
+            .filter(|o| o.is_some())
+            .map(|o| o.unwrap().0)
+            .collect::< Vec::<P> > ();
+
+        for a in closest {
+            remaining.remove(&a);
+            i += 1;
+
+            if i == target {
+                return Some(a);
+            }
+        }
     }
+
+    None
 }
 
 pub fn run(input_str: &String) {
-    assert_eq!((P::new(3, 4), 8), max_visibility(&parse(".#..#\n.....\n#####\n....#\n...##")).unwrap());
-    assert_eq!((P::new(5, 8), 33), max_visibility(&parse("......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####")).unwrap());
-    assert_eq!((P::new(1, 2), 35), max_visibility(&parse("#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.")).unwrap());
-    assert_eq!((P::new(6, 3), 41), max_visibility(&parse(".#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..")).unwrap());
-    assert_eq!((P::new(11, 13), 210), max_visibility(&parse(".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##")).unwrap());
+    let test = vec![
+        ".#..#\n.....\n#####\n....#\n...##",
+        "......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####",
+        "#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.",
+        ".#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..",
+        ".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##",
+    ];
 
-    let max = max_visibility(&parse(&input_str)).unwrap();
-    println!("Max: {:?}, len: {}", max.0, max.1);
+    // Part 1
+    assert_eq!((P::new(3, 4), 8), max_visibility(&parse(test[0])).unwrap());
+    assert_eq!((P::new(5, 8), 33), max_visibility(&parse(test[1])).unwrap());
+    assert_eq!((P::new(1, 2), 35), max_visibility(&parse(test[2])).unwrap());
+    assert_eq!((P::new(6, 3), 41), max_visibility(&parse(test[3])).unwrap());
+    assert_eq!((P::new(11, 13), 210), max_visibility(&parse(test[4])).unwrap());
+
+    let (a, visible) = max_visibility(&parse(&input_str)).unwrap();
+    println!("Part 1: ({}, {}), num visible: {}", a.re, a.im, visible);
+
+    // Part 2
+    assert_eq!(P::new(11, 12), lazer(P::new(11, 13), &parse(test[4]), 1).unwrap());
+    assert_eq!(P::new(12, 1), lazer(P::new(11, 13), &parse(test[4]), 2).unwrap());
+    assert_eq!(P::new(16, 0), lazer(P::new(11, 13), &parse(test[4]), 20).unwrap());
+    assert_eq!(P::new(8, 2), lazer(P::new(11, 13), &parse(test[4]), 200).unwrap());
+    
+    let a = lazer(a, &parse(&input_str), 200).unwrap();
+    println!("Part 2: ({}, {})", a.re, a.im);
 }
